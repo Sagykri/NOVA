4f9443f407d0b1e61a1a3a1a44710f9f22d6627b
unknown <sagykri@gmail.com>
2023-04-03 16:50:55 +0300

    removing params that are not in use

--- src/models/neuroself/configs/run_config_example.py
+++ src/models/neuroself/configs/run_config_example.py
@@ -8,17 +8,13 @@ class RunConfigExample(NeuroselfConfig):
         super(NeuroselfConfig, self).__init__()
         
         self.MARKERS_TO_EXCLUDE = ['DAPI', 'lysotracker', 'Syto12']
-        self.CELL_LINES = None
-        self.CONDITIONS = None
+        self.CELL_LINES = ['WT']
+        self.CONDITIONS = ['unstressed', 'stressed']
         self.SPLIT_DATA = True
         self.DATA_SET_TYPE = 'test'
-        self.TRAIN_CELL_LINES = ['WT']
-        self.TEST_CELL_LINES = ['WT', "TDP43", "OPTN", "FUS", "TBK1"]
-        self.TRAIN_CONDITIONS = ["unstressed"]
-        self.TEST_CONDITIONS = ["unstressed", "stressed"]
         self.MARKERS_FOR_DOWNSAMPLE = None
         self.TRAIN_PCT = 0.7
         self.SHUFFLE = True
-        self.ADD_LINE_TO_LABEL = True
+        self.ADD_LINE_TO_LABEL = False
         self.ADD_TYPE_TO_LABEL = False
         self.ADD_CONDITION_TO_LABEL = True 
