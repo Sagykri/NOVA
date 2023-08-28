3eb10438d6039c5ba07536f41d5f66b1280e105d
noamru <noamru@access.wexac.weizmann.ac.il>
2023-08-28 15:00:37 +0300

    support no_ds pp

--- src/preprocessing/configs/spd_batch5.py
+++ src/preprocessing/configs/spd_batch5.py
@@ -14,3 +14,15 @@ class SPD_Batch5(SPDPreprocessingConfig):
         self.OUTPUT_FOLDERS = [os.path.join(self.PROCESSED_SUBFOLDER_ROOT, "batch5")]
          
         self.WITH_NUCLEUS_DISTANCE = False
+
+
+class SPD_Batch5NODS(SPDPreprocessingConfig):
+    def __init__(self):
+        super().__init__()
+        
+        self.INPUT_FOLDERS = [os.path.join(self.RAW_SUBFOLDER_ROOT, "batch5")]
+        self.OUTPUT_FOLDERS = [os.path.join(self.PROCESSED_SUBFOLDER_ROOT, "batch5_16bit_no_downsample")]
+        self.TO_DOWNSAMPLE = False
+        self.TILE_WIDTH = 128 
+        self.TILE_HEIGHT = 128
+        self.LOGS_FOLDER = os.path.join(self.OUTPUTS_SUBSUBFOLDER,'logs',"no_downsample")
