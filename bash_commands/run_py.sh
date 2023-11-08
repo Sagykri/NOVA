cd74f716934b11c4f5c0950c7ab6783d6ad63fb6
nancyy <nancyy@access3.wexac.weizmann.ac.il>
2023-11-08 16:04:03 +0200

    update usage example

--- bash_commands/run_py.sh
+++ bash_commands/run_py.sh
@@ -6,7 +6,7 @@
 # ./bash_commands/run_py.sh ./tests/test_gpu  -m 1000 -g
 
 # Preprocessing of single batch
-# ./bash_commands/run_py.sh ./src/runables/preprocessing -g -m 20000 -b 10 -a ./src/preprocessing/configs/spd_batch3/SPD_Batch3  
+# ./bash_commands/run_py.sh ./src/runables/preprocessing -g -m 20000 -b 10 -a ./src/preprocessing/configs/spd_batch3/SPD_Batch3  -j preprocessing_SPD_Batch3
 
 # Training neuroself:
 # ./bash_commands/run_py.sh ./src/runables/training -g -m 40000 -b 47 -a ./src/models/neuroself/configs/model_config/TLNeuroselfB78NoDSModelConfig ./src/datasets/configs/training_data_config/B78TrainDatasetConfig -j training_neuroself
@@ -17,14 +17,14 @@
 # Eval model:
 # ./bash_commands/run_py.sh ./src/runables/eval_model -g -m 10000 -b 25 -a ./src/models/cytoself_vanilla/configs/config/CytoselfModelConfig ./src/datasets/configs/train_config/TrainOpenCellDatasetConfig -q gpu-short -j eval_cytoself
 
-# Generate embeddings:
+# Generate embeddings (vqvec2):
 # ./bash_commands/run_py.sh ./src/runables/generate_embeddings -g -m 40000 -b 40 -a ./src/models/neuroself/configs/model_config/TLNeuroselfB78NoDSModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsB9DatasetConfig -j generate_embeddings_vq2
 
 # Generate vqindhist embeddings (spectral_features)
 # ./bash_commands/run_py.sh ./src/runables/generate_spectral_features -g -m 40000 -b 40 -a ./src/models/neuroself/configs/model_config/TLNeuroselfNiemannPickB14ModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsNPB3DatasetConfig -j indhist_NP_3 -q high_gpu_gsla
 
 # Generate UMAP0s
-# ./bash_commands/run_py.sh ./src/runables/generate_umaps -g -m 40000 -b 10 -q high_gpu_gsla -a ./src/models/cytoself_vanilla/configs/config/CytoselfModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsU2OSDatasetConfig /home/labs/hornsteinlab/Collaboration/MOmaps/outputs/figures/U2OS
+# ./bash_commands/run_py.sh ./src/runables/generate_umaps -g -m 40000 -b 10 -q high_gpu_gsla -a ./src/models/cytoself_vanilla/configs/config/CytoselfModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsU2OSDatasetConfig /home/labs/hornsteinlab/Collaboration/MOmaps/outputs/figures/U2OS -j gen_umaps
 
 # Generate UMAP2s - Synthetic Multiplexing
 # ./bash_commands/run_py.sh ./src/runables/run_synthetic_multiplexing -g -m 30000 -b 10  -a ./src/models/cytoself_vanilla/configs/config/CytoselfModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsU2OSDatasetConfig /home/labs/hornsteinlab/Collaboration/MOmaps/outputs/figures/U2OS -j SM_U2OS
