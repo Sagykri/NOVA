4115e0e5d2aad4f901de1ddee101e33b0e61a5e0
nancyy <nancyy@access3.wexac.weizmann.ac.il>
2023-11-08 14:31:23 +0200

    Updating usage examples for all utilities in MOmaps

--- bash_commands/run_py.sh
+++ bash_commands/run_py.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Usage example:
+
+# Test GPU
+# ./bash_commands/run_py.sh ./tests/test_gpu  -m 1000 -g
+
 # Preprocessing of single batch
 # ./bash_commands/run_py.sh ./src/runables/preprocessing -g -m 20000 -b 10 -a ./src/preprocessing/configs/spd_batch3/SPD_Batch3  
 
@@ -19,6 +23,14 @@
 # Generate vqindhist embeddings (spectral_features)
 # ./bash_commands/run_py.sh ./src/runables/generate_spectral_features -g -m 40000 -b 40 -a ./src/models/neuroself/configs/model_config/TLNeuroselfNiemannPickB14ModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsNPB3DatasetConfig -j indhist_NP_3 -q high_gpu_gsla
 
+# Generate UMAP0s
+# ./bash_commands/run_py.sh ./src/runables/generate_umaps -g -m 40000 -b 10 -q high_gpu_gsla -a ./src/models/cytoself_vanilla/configs/config/CytoselfModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsU2OSDatasetConfig /home/labs/hornsteinlab/Collaboration/MOmaps/outputs/figures/U2OS
+
+# Generate UMAP2s - Synthetic Multiplexing
+# ./bash_commands/run_py.sh ./src/runables/run_synthetic_multiplexing -g -m 30000 -b 10  -a ./src/models/cytoself_vanilla/configs/config/CytoselfModelConfig ./src/datasets/configs/embeddings_data_config/EmbeddingsU2OSDatasetConfig /home/labs/hornsteinlab/Collaboration/MOmaps/outputs/figures/U2OS -j SM_U2OS
+
+# Test GPU
+# ./bash_commands/run_py.sh ./tests/test_gpu  -m 1000 -g
 
 # Required param
 py_name=$1
