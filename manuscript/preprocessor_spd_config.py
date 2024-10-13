0ad1cc53404fde916d3b44fca03b59d625bc0875
noamrud@gmail.com <noamrud@gmail.com>
2024-10-13 17:18:39 +0300

    fix qc log savepath;

--- manuscript/preprocessor_spd_config.py
+++ manuscript/preprocessor_spd_config.py
@@ -16,34 +16,39 @@ class NeuronsPreprocessingConfig(SPDPreprocessingBaseConfig):
     def __init__(self):
         super().__init__()
         self.MARKERS_FOCUS_BOUNDRIES_PATH =  os.path.join(os.getenv("NOVA_HOME"), 'manuscript', 'markers_focus_boundries', 'markers_focus_boundries_spd.csv')
-        
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "neurons")
+
 class Batch6(NeuronsPreprocessingConfig):
     def __init__(self):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "batch6")]
         self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch6")]
-        
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch6")
+       
 class Batch7(NeuronsPreprocessingConfig):
     def __init__(self):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "batch7")]
         self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch7")]
-        
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch7")
+       
 class Batch8(NeuronsPreprocessingConfig):
     def __init__(self):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "batch8")]
         self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch8")]
-        
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch8")
+       
 class Batch9(NeuronsPreprocessingConfig):
     def __init__(self):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "batch9")]
         self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch9")]
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch9")
         
 ###################
 #       dNLS      #
@@ -53,6 +58,7 @@ class dNLSPreprocessingConfig(SPDPreprocessingBaseConfig):
     def __init__(self):
         super().__init__()
         self.MARKERS_FOCUS_BOUNDRIES_PATH =  os.path.join(os.getenv("NOVA_HOME"), 'manuscript', 'markers_focus_boundries', 'markers_focus_boundries_dNLS.csv')
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "deltaNLS")
 
 
 class dNLS_Batch3(dNLSPreprocessingConfig):
@@ -61,18 +67,21 @@ class dNLS_Batch3(dNLSPreprocessingConfig):
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "deltaNLS_sort", "batch3")]
         self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "deltaNLS","batch3")]
-        
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch3")
+
 class dNLS_Batch4(dNLSPreprocessingConfig):
     def __init__(self):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "deltaNLS_sort", "batch4")]
         self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "deltaNLS","batch4")]
-        
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch4")
+       
 class dNLS_Batch5(dNLSPreprocessingConfig):
     def __init__(self):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "deltaNLS_sort", "batch5")]
         self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "deltaNLS","batch5")]
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch5")
         
