1179ac616bd46d3abb30a9b28a0743394b2bb1f1
galaviram <galaviram90@gmail.com>
2025-04-23 18:47:35 +0300

    remove rep from label in training

--- src/models/trainers/trainer_contrastive.py
+++ src/models/trainers/trainer_contrastive.py
@@ -24,7 +24,6 @@ class _LabelInfo:
         self.batch:str = label_utils.get_batches_from_labels(__labels_np, dataset_config)[0]
         self.cell_line_cond:str = label_utils.get_cell_lines_conditions_from_labels(__labels_np, dataset_config)[0]
         self.marker:str = label_utils.get_markers_from_labels(__labels_np, dataset_config)[0]
-        self.rep:str = label_utils.get_reps_from_labels(__labels_np, dataset_config)[0]
         self.index:int = index
 
 class TrainerContrastive(TrainerBase):
