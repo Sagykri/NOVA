edf101b7cb56c94d42d91e26ef7c7db604165e3c
ozsharfr <oz.sharabi@mail.huji.ac.il>
2025-05-20 19:31:57 +0300

    Fix for 80pct - wrong path

--- manuscript/preprocessor_opera_config_80pct.py
+++ manuscript/preprocessor_opera_config_80pct.py
@@ -8,8 +8,8 @@ class Opera18DaysReimagedPreprocessingBaseConfig(PreprocessingConfig):
         super().__init__()
         
         self.RAW_FOLDER_ROOT = os.path.join(self.RAW_FOLDER_ROOT, 'Opera18DaysReimaged_sorted')
-        self.PROCESSED_FOLDER_ROOT = os.path.join(self.PROCESSED_FOLDER_ROOT, "Opera18DaysReimaged")
-        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "Opera18Days_Reimaged")
+        self.PROCESSED_FOLDER_ROOT = os.path.join(self.PROCESSED_FOLDER_ROOT, "Opera18DaysReimaged80pct")
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "Opera18Days_Reimaged80pct")
         self.PREPROCESSOR_CLASS_PATH = os.path.join("src", "preprocessing", "preprocessors", "preprocessor_opera", "OperaPreprocessor")
         self.MARKERS_FOCUS_BOUNDRIES_PATH =  os.path.join(os.getenv("NOVA_HOME"), 'manuscript', 'markers_focus_boundries', 'markers_focus_boundries_opera18days_reimaged.csv')
 
@@ -18,8 +18,8 @@ class NeuronsD18_Batch1(Opera18DaysReimagedPreprocessingBaseConfig):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "batch1")]
-        self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch180pct")]
-        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch180pct")
+        self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch1")]
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch180")
         
         
 class NeuronsD18_Batch2(Opera18DaysReimagedPreprocessingBaseConfig):
@@ -27,5 +27,5 @@ class NeuronsD18_Batch2(Opera18DaysReimagedPreprocessingBaseConfig):
         super().__init__()
         
         self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, "batch2")]
-        self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch280pct")]
-        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch280pct")
+        self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, "batch2")]
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "batch2")
