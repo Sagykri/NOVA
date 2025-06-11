0fc29ea3a428d15ebb259a97b566c7a85e686d72
SagyKri <sagykri@gmail.com>
2025-06-11 10:03:38 +0300

    rename the output folder for the model eval

--- manuscript/run_compare_models.sh
+++ manuscript/run_compare_models.sh
@@ -1,7 +1,7 @@
 echo "NOVA_HOME:" $NOVA_HOME
 
 mem=100000
-output_dir="$NOVA_HOME/outputs/model_evaluations_090625_80pct"
+output_dir="$NOVA_HOME/outputs/model_evaluations"
 models_path_filepath="$NOVA_HOME/manuscript/models_path.json"
 embeddings_path="./manuscript/embeddings_config_80pct"
 k=10
@@ -16,7 +16,7 @@ $NOVA_HOME/runnables/run.sh $NOVA_HOME/runnables/evaluate_models -g -b 1 -m $mem
 $NOVA_HOME/runnables/run.sh $NOVA_HOME/runnables/evaluate_models -g -b 1 -m $mem -j eval_dNLS4 -a $output_dir $embeddings_path/"EmbeddingsdNLSB4DatasetConfig_NoRepNoBatchNoDAPI" $models_path_filepath $k $neg_k 0.3
 $NOVA_HOME/runnables/run.sh $NOVA_HOME/runnables/evaluate_models -g -b 1 -m $mem -j eval_dNLS5 -a $output_dir $embeddings_path/"EmbeddingsdNLSB5DatasetConfig_NoRepNoBatchNoDAPI" $models_path_filepath $k $neg_k 0.5
 
-neurons
+# neurons
 $NOVA_HOME/runnables/run.sh $NOVA_HOME/runnables/evaluate_models -g -b 1 -m $mem -j eval_neurons4 -a $output_dir $embeddings_path/"EmbeddingsB4DatasetConfig_NoRepNoBatchNoDAPI" $models_path_filepath $k $neg_k $sample_fraction
 $NOVA_HOME/runnables/run.sh $NOVA_HOME/runnables/evaluate_models -g -b 1 -m $mem -j eval_neurons5 -a $output_dir $embeddings_path/"EmbeddingsB5DatasetConfig_NoRepNoBatchNoDAPI" $models_path_filepath $k $neg_k $sample_fraction
 $NOVA_HOME/runnables/run.sh $NOVA_HOME/runnables/evaluate_models -g -b 1 -m $mem -j eval_neurons6 -a $output_dir $embeddings_path/"EmbeddingsB6DatasetConfig_NoRepNoBatchNoDAPI" $models_path_filepath $k $neg_k $sample_fraction
