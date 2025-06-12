c9b2393bec902510a1eb18e03e01213d587ff16d
galaviram <galaviram90@gmail.com>
2025-06-12 17:20:59 +0300

    preproc config for iAstrocytes

---
+++ manuscript/preprocessing_config_iAstrocytes.py
@@ -0,0 +1,20 @@
+import os
+import sys
+sys.path.insert(1, os.getenv("NOVA_HOME"))
+from src.preprocessing.preprocessing_config import PreprocessingConfig
+
+class PreprocessingBaseConfigiAstrocytes(PreprocessingConfig):
+    def __init__(self):
+        super().__init__()
+        self.RAW_FOLDER_ROOT = os.path.join(self.RAW_FOLDER_ROOT, 'John', 'iAstrocytes', 'ordered')
+        self.PROCESSED_FOLDER_ROOT = os.path.join(self.PROCESSED_FOLDER_ROOT, "iAstrocytes")
+        self.OUTPUTS_FOLDER = os.path.join(self.OUTPUTS_FOLDER, "iAstrocytes")
+        self.PREPROCESSOR_CLASS_PATH = os.path.join("src", "preprocessing", "preprocessors", "preprocessor_opera", "OperaPreprocessor")        
+
+        self.MARKERS_FOCUS_BOUNDRIES_PATH =  os.path.join(os.getenv("NOVA_HOME"), 'manuscript', 'markers_focus_boundries', 'markers_focus_boundries_iAstrocytes.csv')
+        
+        self.CELL_LINES = ['WT', 'C9']
+        self.CONDITIONS = ['Untreated']
+        self.REPS = [f"rep{i}" for i in range(1,4)]
+        self.INPUT_FOLDERS = [os.path.join(self.RAW_FOLDER_ROOT, f"batch{i}") for i in range(1,2)]
+        self.PROCESSED_FOLDERS = [os.path.join(self.PROCESSED_FOLDER_ROOT, f"batch{i}") for i in range(1,2)]
