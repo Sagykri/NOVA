e31c7a614e62279a0a1d9a82f3d266bce7c7d067
galaviram <galaviram90@gmail.com>
2025-09-16 16:19:59 +0300

    option to input plot config to create label map based on the manuscript name

--- runnables/classify_groups.py
+++ runnables/classify_groups.py
@@ -7,14 +7,23 @@ print(f"NOVA_HOME: {os.getenv('NOVA_HOME')}")
 import logging
 from src.common.utils import load_config_file
 from src.classifier.classifier_config import ClassifierConfig
+from src.figures.plot_config import PlotConfig
 from src.analysis.analyzer_classification import AnalyzerClassification
 
 
-def classify_groups(output_folder_path: str, config_class_path: str) -> None:
+def classify_groups(output_folder_path: str, config_class_path: str, config_path_plot: str) -> None:
     logging.info("[classify_groups] start")
 
     classification_config:ClassifierConfig = load_config_file(config_class_path, 'data')
     classification_config.OUTPUTS_FOLDER = output_folder_path
+
+    if config_path_plot is not None:
+        config_plot:PlotConfig = load_config_file(config_path_plot, 'plot')
+        color_mappings = config_plot.COLOR_MAPPINGS  
+        # build label_map 
+        label_map = {k: v["alias"] for k, v in color_mappings.items()}
+        classification_config.label_map = label_map
+        logging.info(f"Using label_map from plot config")
 
     d = AnalyzerClassification(classification_config, output_folder_path)
     d.calculate_and_plot()
@@ -29,7 +38,12 @@ if __name__ == "__main__":
             )
         output_folder_path = sys.argv[1]
         config_class_path = sys.argv[2]
-        classify_groups(output_folder_path, config_class_path)
+        if len(sys.argv) > 3: 
+            # optional plot config for label mapping
+            config_path_plot = sys.argv[3]
+        else:
+            config_path_plot = None
+        classify_groups(output_folder_path, config_class_path, config_path_plot)
     except Exception as e:
         logging.exception(str(e))
         raise
